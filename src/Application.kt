79b5895dba632a82210aebb74a0a434c6706f020
Marek Lowy <mareklowy@protonmail.ch>
2019-10-09 19:04:06 +0200

    First try to setup serving on our apache server

--- src/Application.kt
+++ src/Application.kt
@@ -17,6 +17,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
     install(ContentNegotiation) {
         gson {
         }
